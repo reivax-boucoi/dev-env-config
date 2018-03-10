9c3990fe8fc0a9ae011cf7dc5f0fb6cf084a1deb
jucom <julien.combattelli@hotmail.com>
2018-03-10 17:16:30 +0100

    Fix typo in setenv-test.sh
    Signed-off-by: jucom <julien.combattelli@hotmail.com>

--- ubuntu-config/setenv-test.sh
+++ ubuntu-config/setenv-test.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-sudo apt install git curl python-pip
+sudo apt install git curl python-pip vim
 
 sudo pip install powerline-status
 
-POWERLINE_STATUS_LOCATION=$(pip show powerline status | grep Location | cut -d' ' -f2)
+POWERLINE_STATUS_LOCATION=$(pip show powerline-status | grep Location | cut -d' ' -f2)
 
 wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
 wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
@@ -18,7 +18,7 @@ echo "
 powerline-daemon -q
 POWERLINE_BASH_CONTINUATION=1
 POWERLINE_BASH_SELECT=1
-. $POWERLINE_STATUS_LOCATION/powerline/binding/bash/powerline.sh " >> ~/.bashrc
+. $POWERLINE_STATUS_LOCATION/powerline/bindings/bash/powerline.sh " >> ~/.bashrc
 
 echo "
 \" Powerline Font path
@@ -28,21 +28,23 @@ set t_Co=256 " >> ~/.vimrc
 
 sudo apt-get install zsh
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) && exit"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+sed -i "s/ZSH_THEME=\".*\"/ZSH_THEME=\"agnoster\"/" ~/.zshrc
 echo "
 # Custom settings
-ZSH_THEME="agnoster"
 unsetopt nomatch " >> ~/.zshrc
 
 ZSH_LOCATION=$(which zsh)
-chsh -s ZSH_LOCATION
+chsh -s $ZSH_LOCATION
 
 git clone https://github.com/powerline/fonts.git --depth=1
 cd fonts
 ./install.sh
 mkdir -p ~/.config/fontconfig/conf.d
 mv fontconfig/50-enable-terminess-powerline.conf ~/.config/fontconfig/conf.d/
+cd ..
+rm -rf fonts
 
 exit
 
