ea77f7a9db08aeb98937af6cf7ddfdd33523a8c2
jucom <julien.combattelli@hotmail.com>
2018-04-27 11:39:01 +0200

    Update ubuntu-config/setenv.sh

--- ubuntu-config/setenv.sh
+++ ubuntu-config/setenv.sh
@@ -2,45 +2,74 @@
 
 LOCAL_DIR=$(dirname $0)
 
-echo Adding ubuntu-toolchain-r PPA...
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-echo Upgrading the system...
 sudo apt update
 sudo apt upgrade -y
-echo Installing native-toolchain...
-sudo apt install -y build-essential git cmake gcc-6 g++-6 gcc-7 g++-7 vim zsh zsh-syntax-highlighting
+sudo apt install -y \
+	build-essential gcc-7 g++-7 \
+	cmake git curl python-pip \
+	vim zsh zsh-syntax-highlighting
 
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 100 --slave /usr/bin/g++ g++ /usr/bin/g++-6
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 100 --slave /usr/bin/g++ g++ /usr/bin/g++-7
 
-echo Installing powerline...
-sudo apt install -y python-pip
 sudo pip install powerline-status
-sudo apt install -y fonts-powerline
 
-POWERLINE_DIR=$(pip show powerline-status | grep Location | cut -d' ' -f2)/powerline
-echo Powerline location: $POWERLINE_DIR
+POWERLINE_STATUS_LOCATION=$(pip show powerline-status | grep Location | cut -d' ' -f2)
 
-echo Updating ~/.vimrc
-echo \" Powerline Font Path >> ~/.vimrc
-echo set rtp+=$POWERLINE_DIR/bindings/vim/ >> ~/.vimrc
-echo set laststatus=2 >> ~/.vimrc
-echo set t_Co=256 >> ~/.vimrc
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
 
-echo Updating ~/.zshrc
-echo ZSH_THEME="agnoster" >> ~/.zshrc
-echo DEFAULT_USER=$(whoami) >> ~/.zshrc
-echo unsetopt nomatch >> ~/.zshrc
-echo source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh >> ~/.zshrc
+sudo mv PowerlineSymbols.otf /usr/share/fonts/
+sudo fc-cache -vf /usr/share/fonts/
+sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
 
-echo Setting Konsole profile...
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+mkdir -p ~/.config/fontconfig/conf.d
+mv fontconfig/50-enable-terminess-powerline.conf ~/.config/fontconfig/conf.d/
+cd ..
+rm -rf fonts
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo "
+# Powerline Font path
+powerline-daemon -q
+POWERLINE_BASH_CONTINUATION=1
+POWERLINE_BASH_SELECT=1
+. $POWERLINE_STATUS_LOCATION/powerline/bindings/bash/powerline.sh " >> ~/.bashrc
+
+echo "
+\" General setting
+syntax on
+set number
+set showmatch
+\" Powerline Font path
+set rtp+=$POWERLINE_STATUS_LOCATION/powerline/bindings/vim/
+set laststatus=2
+set t_Co=256 
+\" Indentation settings
+set expandtab
+set shiftwidth=4
+set softtabstop=4
+set cindent
+set cinoptions=:0,l1,g0,(0,W4,N-s
+" >> ~/.vimrc
+
+sed -i "s/ZSH_THEME=\".*\"/ZSH_THEME=\"agnoster\"/" ~/.zshrc
+echo "
+# Custom settings
+DEFAULT_USER=$(whoami)
+unsetopt nomatch " >> ~/.zshrc
+
 mkdir -p ~/.local/share/konsole
 cp $LOCAL_DIR/local/share/konsole/JucomProfile.profile ~/.local/share/konsole/
 cp $LOCAL_DIR/local/share/konsole/Solarized.colorscheme ~/.local/share/konsole/
 
-echo Installing Oh-My-Zsh...
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+# Done by oh-my-zsh install script
+#ZSH_LOCATION=$(which zsh)
+#chsh -s $ZSH_LOCATION
 
-#read -rsp $'Press any key to continue...\n' -n1 key
-#exit
+exit
 
