c75e894343097f1d7b024782f13e12b3ceb350d4
Julien Combattelli <julien.combattelli@hotmail.com>
2018-12-07 17:24:00 +0100

    Add simpler version of ubuntu setup script

---
+++ ubuntu-config-test/local/share/konsole/JucomProfile.profile
@@ -0,0 +1,16 @@
+[Appearance]
+AntiAliasFonts=true
+BoldIntense=true
+ColorScheme=Solarized
+Font=Hack,12,-1,0,50,0,0,0,0,0,Regular
+
+[General]
+Name=JucomProfile
+Parent=FALLBACK/
+Command=/usr/bin/zsh
+
+[Keyboard]
+KeyBindings=default
+
+[Scrolling]
+HistoryMode=2
---
+++ ubuntu-config-test/setenv.sh
@@ -0,0 +1,53 @@
+#! /bin/bash
+
+LOCAL_DIR=$(dirname $0)
+
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y \
+	build-essential gcc-7 g++-7 \
+	cmake git \
+	vim zsh tmux powerline powerline-fonts
+
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 100 --slave /usr/bin/g++ g++ /usr/bin/g++-7
+
+# Install Oh-My-Zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# Install Zsh plugins
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
+
+# Setting up git
+git config --global color.ui true
+git config --global core.editor "vim"
+git config --global user.email "julien.combattelli@hotmail.com"
+git config --global user.name "Julien Combattelli"
+
+# Setting up tmux
+cp .tmux.conf ~
+
+# Setting up bashrc
+cat .bashrc.append >> ~/.bashrc
+
+# Setting up vimrc
+cat .vimrc.append >> ~/.vimrc
+
+# Setting up zshrc
+sed -i "s/ZSH_THEME=\".*\"/ZSH_THEME=\"agnoster\"/" ~/.zshrc
+cat .zshrc.append >> ~/.zshrc
+
+# Setting up konsole
+mkdir -p ~/.local/share/konsole
+cp $LOCAL_DIR/local/share/konsole/JucomProfile.profile ~/.local/share/konsole/
+cp $LOCAL_DIR/local/share/konsole/Solarized.colorscheme ~/.local/share/konsole/
+
+# Done by oh-my-zsh install script
+#ZSH_LOCATION=$(which zsh)
+#chsh -s $ZSH_LOCATION
+
+exit
+
